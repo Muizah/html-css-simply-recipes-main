3a13d95680eca715343d5da561be4428eb4dc0ac
Muizah <mapampa7@gmail.com>
2022-08-29 15:07:08 +0100

    work on footer

--- html-css-simply-recipes-main/starter/js/app.js
+++ html-css-simply-recipes-main/starter/js/app.js
@@ -1,19 +1,19 @@
 const getElement = (selector) => {
-  const element = document.querySelector(selector)
+  const element = document.querySelector(selector);
 
-  if (element) return element
+  if (element) return element;
   throw Error(
     `Please double check your class names, there is no ${selector} class`
-  )
-}
+  );
+};
 
-const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
+const links = getElement(".nav-links");
+const navBtnDOM = getElement(".nav-btn");
 
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
+navBtnDOM.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
 
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+const date = getElement("#date");
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
